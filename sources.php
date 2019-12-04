ac64ec0f7d0faf4274774dab1a35dcafb75203dc
Ed Baker <edwbaker@gmail.com>
2019-12-04 17:06:31 +0100

    Create sources.php

---
+++ sources.php
@@ -0,0 +1,9 @@
+<?php
+
+$sources = array();
+
+$sources['bioacoustica'] = array(
+  'name' => 'BioAcoustica',
+  'import' => 'http://bio.acousti.ca/audioblast/annotations',
+  'contact' => 'http://bio.acousti.ca/contact/1'
+);
